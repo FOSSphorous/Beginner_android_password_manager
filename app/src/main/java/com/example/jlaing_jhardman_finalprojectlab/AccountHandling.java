506ddc86c23b4350590819397da973fdc5a31ae6
FOSSphorous <fossphorous@outlook.com>
2020-12-08 09:30:44 -0700

    Connected login.xml to functions in MainActivity.java, moved buttons a bit. Added a check for existing accounts method to AccountHandling.java: finished up login.xml and activity_main.xml dealings in MainActivity.java. Time to move on to the other half.

--- app/src/main/java/com/example/jlaing_jhardman_finalprojectlab/AccountHandling.java
+++ app/src/main/java/com/example/jlaing_jhardman_finalprojectlab/AccountHandling.java
@@ -1,8 +1,11 @@
 package com.example.jlaing_jhardman_finalprojectlab;
 
 import java.util.Random;
+import java.util.Arrays;
+import java.util.List;
 
 public class AccountHandling {
+    public DataStorage dataStorage = new DataStorage();
 
     public String generateRandomString (int charLength) {
         char[] characterList = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0','!','@','#','$','%','^','&','*','(',')','?'};
@@ -18,5 +21,11 @@ public class AccountHandling {
         Random randGenerator = new Random();
         int randNum = randGenerator.nextInt(maxBound - minBound + 1) + minBound;
         return randNum;
+    }
+
+    public boolean accountCheck(String[] dictionary, String searchItem) {
+        List<String> list = Arrays.asList(dictionary);
+        final boolean contains = list.contains(searchItem);
+        return contains;
     }
 }
--- app/src/main/java/com/example/jlaing_jhardman_finalprojectlab/MainActivity.java
+++ app/src/main/java/com/example/jlaing_jhardman_finalprojectlab/MainActivity.java
@@ -7,11 +7,83 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
 public class MainActivity extends AppCompatActivity {
+    public AccountHandling accountHandling = new AccountHandling();
+    public DataStorage dataStorage = new DataStorage();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+
+    public void onGenUsernameIntro (View v){
+        EditText setupUsername = findViewById(R.id.txtSetupUsername);
+        setupUsername.setText(accountHandling.generateRandomString(8));
+    }
+
+    public void onGenPasswordIntro(View v){
+        EditText setupPassword = findViewById(R.id.txtSetupPassword);
+        setupPassword.setText(accountHandling.generateRandomString(8));
+    }
+
+    public void onAccountCreate(View v){
+        EditText setupPassword = findViewById(R.id.txtSetupPassword);
+        EditText setupUsername = findViewById(R.id.txtSetupUsername);
+
+        String Username = setupUsername.getText().toString();
+        String Password = setupPassword.getText().toString();
+
+        if(Username.contentEquals("") || Password.contentEquals("")){
+            TextView ErrorView = findViewById(R.id.viewError);
+            ErrorView.setText("Please make sure to make an entry in all available fields.");
+        } else {
+            String appSecret = Username + ":" + Password;
+            ArrayList<String> tmpList = new ArrayList<>(Arrays.asList(dataStorage.appSecrets));
+            tmpList.add(appSecret);
+            dataStorage.appSecrets = tmpList.toArray(new String[dataStorage.appSecrets.length + 1]);
+            setContentView(R.layout.login);
+        }
+    }
+
+    public void onAccountExists(View v){
+        if(dataStorage.appSecrets.length == 0){
+            TextView ErrorView = findViewById(R.id.viewError);
+            ErrorView.setText("Could not find an existing account.\nPlease create a new one.");
+        } else {setContentView(R.layout.login);}
+    }
+
+    public void onCreateAccountPress(View v){
         setContentView(R.layout.activity_main);
+    }
+
+    public void onLogin(View v){
+        EditText usernameLogin = findViewById(R.id.txtUsernameLogin);
+        EditText passwordLogin = findViewById(R.id.txtPasswordLogin);
+
+        String proposedSecret = usernameLogin.getText().toString() + ":" + passwordLogin.getText().toString();
+
+        if(accountHandling.accountCheck(dataStorage.appSecrets, proposedSecret)){
+            setContentView(R.layout.account_display);
+        } else {
+            TextView ErrorView = findViewById(R.id.viewErrorTwo);
+            ErrorView.setText("The credentials you entered could not be found.");
+        }
+    }
+
+    public void onDelete(View v){
+        dataStorage.appSecrets = new String[]{};
+        dataStorage.accountNames = new String[]{};
+        dataStorage.accountSecrets = new String[]{};
+
+        setContentView(R.layout.activity_main);
+
+        TextView ErrorView = findViewById(R.id.viewError);
+        ErrorView.setText("Could not find an existing account.\nPlease create a new one.");
     }
 
 }
